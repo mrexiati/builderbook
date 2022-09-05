380d4ae1501bec9635518b1b053f365be24be56d
RishatMamat <reshatma@gmail.com>
2022-09-05 13:28:13 -0600

    Add toolbar to all pages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import App from 'next/app';
 import PropTypes from 'prop-types';
 import React from 'react';
+import Header from '../components/Header';
 
 const propTypes = {
   Component: PropTypes.elementType.isRequired,
@@ -11,7 +12,12 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
 
-    return <Component {...pageProps} />;
+    return (
+      <>
+        <Header {...pageProps} />
+        <Component {...pageProps} />
+      </>
+    );
   }
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Header from '../components/Header';
 
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
@@ -7,7 +6,6 @@ const Index = () => (
       <title> This is the title page</title>
       <meta name="description" content="This is the content of description page" />
     </Head>
-    <Header />
     <p>Content on index page</p>
   </div>
 );

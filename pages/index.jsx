e76354cea82ea4bb919d2c2d9730adfe9dee4e32
RishatMamat <reshatma@gmail.com>
2022-09-15 20:57:00 -0600

    Add getProps

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,33 @@
 import Head from 'next/head';
-import Button from '@mui/material/Button';
+import PropTypes from 'prop-types';
 
-const Index = () => (
+const propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title> This is the title page</title>
       <meta name="description" content="This is the content of description page" />
     </Head>
     <p>Content on index page</p>
+    <p>
+      Email:
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
 
 export default Index;

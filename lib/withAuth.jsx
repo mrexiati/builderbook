cc8a6302135a759de9126141865937ca72bf9d16
RishatMamat <reshatma@gmail.com>
2022-10-16 15:58:31 -0600

    Add withauth HOC

--- lib/withAuth.jsx
+++ lib/withAuth.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Router from 'next/router';
 
-const globalUser = null;
+let globalUser = null;
 
 export default function withAuth(
   BaseComponent,
@@ -22,14 +22,51 @@ export default function withAuth(
 
   class App extends React.Component {
     static async getInitialProps(ctx) {
-      // get props
+      const isFromServer = typeof window === 'undefined';
+      const user = ctx.req ? ctx.req.user : globalUser;
+
+      if (isFromServer && user) {
+        user._id = user._id.toString();
+      }
+
+      const props = { user, isFromServer };
+
+      if (BaseComponent.getInitialProps) {
+        Object.assign(props, (await BaseComponent.getInitialProps(ctx)) || {});
+      }
+
+      return props;
     }
 
     componentDidMount() {
-      // component did mounts
+      const { user, isFromServer } = this.props;
+
+      if (isFromServer) {
+        globalUser = user;
+      }
+
+      if (loginRequired && !logoutRequired && !user) {
+        Router.push('/public/login', 'login');
+
+        return;
+      }
+
+      if (logoutRequired && user) {
+        Router.push('/');
+      }
     }
 
     render() {
+      const user = this.props;
+
+      if (loginRequired && !logoutRequired && !user) {
+        return null;
+      }
+
+      if (logoutRequired && user) {
+        return null;
+      }
+
       return (
         <>
           <BaseComponent {...this.props} />

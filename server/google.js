1162cfae5d77794f30f786790cd931c49b616a79
RishatMamat <reshatma@gmail.com>
2022-10-27 13:05:52 -0600

    Add googleAuth steps

--- server/google.js
+++ server/google.js
@@ -3,6 +3,32 @@ const Strategy = require('passport-google-oauth').OAuth2Strategy;
 const User = require('./models/User');
 
 function setupGoogle({ ROOT_URL, server }) {
+  const verify = async (accessToken, refreshToken, profile, verified) => {
+    let email;
+    let avatarUrl;
+
+    if (profile.email) {
+      email = profile.emails[0].email;
+    }
+
+    if (profile.photos && profile.photos.length > 0) {
+      avatarUrl = profile.photos[0].value.replace('sz=50', 'sz=128');
+    }
+
+    try {
+      const user = await User.signInOrSignUp({
+        googleId: profile.id,
+        email,
+        googleToken: { accessToken, refreshToken },
+        displayName: profile.displayName,
+        avatarUrl,
+      });
+      verified(null, user);
+    } catch (err) {
+      verified(err);
+      console.log(err);
+    }
+  };
   passport.use(
     new Strategy(
       {

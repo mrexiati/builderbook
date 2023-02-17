b4a1b3bb8de7d3a17de9da4b0cf7b5d07edf77c7
RishatMamat <reshatma@gmail.com>
2023-02-16 17:39:21 -0700

    Add chapter schema

--- server/Models/Chapter.js
+++ server/Models/Chapter.js
@@ -3,28 +3,61 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const mongoSchema = new Schema({
-  name: {
+  bookId: {
+    type: Schema.Types.ObjectId,
+    required: true,
+  },
+  isFree: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  title: {
     type: String,
     required: true,
   },
   slug: {
     type: String,
-    required: true,
-    unique: true,
-  },
-  githubRepo: {
-    type: String,
-    required: true,
-  },
-  githubLastCommitSha: String,
-  createdAt: {
-    type: Date,
     require: true,
   },
-  price: {
+  content: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  htmlContent: {
+    type: String,
+    default: '',
+    required: true,
+  },
+  excerpt: {
+    type: String,
+    default: '',
+  },
+  htmlExcerpt: {
+    type: String,
+    default: '',
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+  },
+  githubFilePath: {
+    type: String,
+  },
+  order: {
     type: Number,
     required: true,
   },
+  seoTitle: String,
+  seoDescription: String,
+  sections: [
+    {
+      text: String,
+      level: Number,
+      escaptedText: String,
+    },
+  ],
 });
 
 class ChapterClass {}

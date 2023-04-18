8c131f69a47486c42eaa83a47526846e2ddb6748
RishatMamat <reshatma@gmail.com>
2023-04-17 21:48:11 -0600

    Add read chapter component

--- pages/public/read-chapter.jsx
+++ pages/public/read-chapter.jsx
@@ -7,7 +7,21 @@ import { getChapterDetailApiMethod } from '../../lib/api/public';
 import withAuth from '../../lib/withAuth';
 
 class ReadChapter extends React.Component {
-  constructor(props) {}
+  constructor(props) {
+    super(props);
+
+    const { chapter } = props;
+
+    let htmlContent = '';
+    if (chapter) {
+      htmlContent = chapter.htmlContent;
+    }
+
+    this.state = {
+      chapter,
+      htmlContent,
+    };
+  }
 
   componentDidUpdate(prevProps) {}
 
@@ -15,7 +29,51 @@ class ReadChapter extends React.Component {
 
   renderMainContent() {}
 
-  render() {}
+  render() {
+    const { chapter } = this.state;
+
+    if (!chapter) {
+      return <Head statusCode={404} />;
+    }
+
+    return (
+      <div>
+        <Head>
+          <title>
+            {chapter.title === 'Introduction'
+              ? 'Introduction'
+              : `Chapter ${chapter.order - 1}. ${chapter.title}`}
+          </title>
+          {chapter.seoDescription ? (
+            <meta name="description" content={chapter.seoDescription} />
+          ) : null}
+        </Head>
+
+        <div
+          style={{
+            textAlign: 'left',
+            padding: '0px 10px 20px 30px',
+            position: 'fixed',
+            right: 0,
+            bottom: 0,
+            top: '64px',
+            left: '320px',
+            overflowY: 'auto',
+            overflowX: 'hidden',
+          }}
+        >
+          <div
+            style={{
+              position: 'fixed',
+              top: '80px',
+              left: '15px',
+            }}
+          />
+          {this.renderMainContent()}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default withAuth(ReadChapter, { loginRequired: false });

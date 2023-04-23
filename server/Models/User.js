88332126ae798b0f8b28aeb3119c81d550f94e6f
RishatMamat <reshatma@gmail.com>
2023-04-23 13:37:26 -0600

    Add github models to user

--- server/Models/User.js
+++ server/Models/User.js
@@ -37,13 +37,28 @@ const mongoSchema = new Schema({
     type: Boolean,
     default: false,
   },
+  isGithubConnected: {
+    type: Boolean,
+    default: false,
+  },
+  githubAccessToken: {
+    type: String,
+  },
+  githubId: {
+    type: String,
+    unique: true,
+  },
+  githubUsername: {
+    type: String,
+    unique: true,
+  },
   displayName: String,
   avatarUrl: String,
 });
 
 class UserClass {
   static publicFields() {
-    return ['id', 'displayName', 'email', 'avatarUrl', 'slug', 'isAdmin'];
+    return ['id', 'displayName', 'email', 'avatarUrl', 'slug', 'isAdmin', 'isGithubConnected'];
   }
 
   static async signInOrSignUp({ googleId, email, googleToken, displayName, avatarUrl }) {

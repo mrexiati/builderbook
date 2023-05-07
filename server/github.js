59b8280a3fe9e3056963e2cec2fa0b41539c84be
RishatMamat <reshatma@gmail.com>
2023-05-07 17:22:52 -0600

    Add callback method

--- server/github.js
+++ server/github.js
@@ -12,7 +12,24 @@ const CLIENT_ID = dev ? process.env.GITHUB_TEST_CLIENTID : process.env.GITHUB_LI
 const API_KEY = dev ? process.env.GITHUB_TEST_SECRETKEY : process.env.GITHUB_LIVE_SECRETKEY;
 
 function setupGithub({ server, ROOT_URL }) {
-  const verify = async ({ user, accessToken, profile }) => {};
+  const verify = async ({ user, accessToken, profile }) => {
+    const modifier = {
+      githubId: profile.id,
+      githubAccessToken: accessToken,
+      githubUsername: profile.login,
+      isGithubConnected: true,
+    };
+
+    if (!user.displayName) {
+      modifier.displayName = profile.name || profile.login;
+    }
+
+    if (!user.avatarUrl && profile.avatar_url) {
+      modifier.avatarUrl = profile.avatar_url;
+    }
+
+    await User.updateOne({ _id: user._id }, modifier);
+  };
 
   server.get('/auth/github', (req, res) => {
     if (!req.user || !req.user.isAdmin) {
@@ -37,7 +54,60 @@ function setupGithub({ server, ROOT_URL }) {
     res.redirect(url);
   });
 
-  server.get('/auth/github/callback', async (req, res) => {});
+  server.get('/auth/github/callback', async (req, res) => {
+    if (!req.user || !req.user.isAdmin) {
+      res.redirect(`${ROOT_URL}/login`);
+      return;
+    }
+
+    const { next_url, githubAuthState } = req.session;
+
+    let redirectUrl = ROOT_URL;
+
+    if (next_url && next_url.startsWith('/')) {
+      req.session.next_url = null;
+      redirectUrl = `${ROOT_URL}${next_url}`;
+    }
+
+    if (githubAuthState !== req.query.state) {
+      res.redirect(`${redirectUrl}/admin?error=Wrong request`);
+    }
+
+    try {
+      const response = await fetch('https://github.com/login/oauth/access_token', {
+        mmethod: 'POST',
+        headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
+        body: JSON.stringify({
+          client_id: CLIENT_ID,
+          client_secret: API_KEY,
+          code: req.query.code,
+          state: req.query.state,
+          redirect_uri: `${ROOT_URL}/auth/github/callback`,
+        }),
+      });
+
+      const resData = await response.json();
+
+      const github = new Octokit({
+        auth: resData.access_token,
+        request: { timeout: 10000 },
+      });
+
+      const profile = await github.users.getAuthenticated();
+
+      await verify({
+        user: req.user,
+        accessToken: resData.access_token,
+        profile: profile.data,
+      });
+    } catch (error) {
+      console.error(error.toString());
+
+      res.redirect(`${redirectUrl}/admin?error=${error.toString()}`);
+    }
+
+    res.redirect(`${redirectUrl}/admin`);
+  });
 }
 
 exports.setupGithub = setupGithub;

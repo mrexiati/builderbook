da365ab8f777e7f830a6728cc2efafd37f47f21a
RishatMamat <reshatma@gmail.com>
2024-02-19 16:54:43 -0700

    Add edit book class and methods

---
+++ pages/admin/edit-book.jsx
@@ -0,0 +1,77 @@
+import React from 'react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import PropTypes from 'prop-types'
+
+import withAuth from '../../lib/withAuth'
+import EditBookPage from '../../components/admin/EditBook'
+import {  } from "../../lib/api/admin";
+import notify from '../../lib/notify'
+
+const propTypes = {
+    slug: PropTypes.string.isRequired
+}
+
+class EditBookPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            book: null,
+        }
+    }
+
+    static getInitialProps({query}) {
+        return {slug: query.slug}
+    } 
+
+    async componentDidMount() {
+        NProgress.start();
+
+        
+
+    }
+
+    addBookOnSave = async (data) => {
+        NProgress.start();
+
+        try {
+            const book = await addBookApiMethod(data);
+
+            notify('Saved');
+
+            try {
+                const bookId = book._id;
+
+                await syncBookContentApiMethod({ bookId });
+
+                notify('Synced');
+
+                NProgress.done();
+
+                Router.push(`admin/book-detail?slug=${book.slug}`, `/admin/book-detail/${book.slug}`)
+            } catch (err) {
+                notify(err.message || err.toString());
+
+                NProgress.done()
+            }
+        }catch (err) {
+            notify(err.message || err.toString());
+
+            NProgress.done()
+        }
+
+    }
+
+    render() {
+        return(
+            <div style={{ padding: '10px 45px'}}>
+                <EditBookPage onSave={this.addBookOnSave} />
+            </div>
+        )
+    }
+}
+
+EditBookPage.propTypes = propTypes
+
+export default withAuth(EditBookPage)
